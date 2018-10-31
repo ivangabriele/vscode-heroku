a28de29e54d90f27997996bc6b9492b15efcd5fb
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-10-31 21:06:40 +0100

    components/HerokuStatus: add statuses & check "heroku" command

--- src/components/HerokuStatus.ts
+++ src/components/HerokuStatus.ts
@@ -1,14 +1,50 @@
 import { StatusBarAlignment, StatusBarItem, window } from 'vscode'
+import { sync as commandExistsSync } from 'command-exists'
+
+// Icons: https://octicons.github.com
+const STATUS = {
+  FAILED: {
+    icon: 'circle-slash',
+    message: 'Heroku',
+    tooltip: `The last Heroku deployment failed.`,
+  },
+  IN_PROGRESS: {
+    icon: 'clock',
+    message: 'Heroku',
+    tooltip: `Heroku deployment in progress...`,
+  },
+  SUCCESSFUL: {
+    icon: 'check',
+    message: 'Heroku',
+    tooltip: `The last Heroku deployment succeeded.`,
+  },
+  SYNCING: {
+    icon: 'sync',
+    message: 'Heroku',
+    tooltip: `Fetching the current Heroku deployment status...`,
+  },
+  UNAVAILABLE: {
+    icon: 'alert',
+    message: 'Heroku CLI unavailable',
+    tooltip: `The "heroku" command doesn't seem available. Did you install Heroku CLI ?`,
+  },
+}
 
 export default class HerokuStatus {
   private statusBarItem: StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left)
 
   constructor() {
-    this.updateStatus()
+    if (commandExistsSync('heroku')) {
+      this.setStatusTo(STATUS.SYNCING)
+    } else {
+      this.setStatusTo(STATUS.UNAVAILABLE)
+    }
+
+    this.statusBarItem.show()
   }
 
-  private updateStatus() {
-    this.statusBarItem.text = 'Heroku $(sync)'
-    this.statusBarItem.show()
+  private setStatusTo(status) {
+    this.statusBarItem.text = `$(${status.icon}) ${status.message}`
+    this.statusBarItem.tooltip = status.tooltip
   }
 }
