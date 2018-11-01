33195d7bcfa0546586036920dafc19007a5baf25
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-01 23:27:30 +0100

    helpers/shortenMomentOutput: fix output replacements

--- src/helpers/shortenMomentOutput.ts
+++ src/helpers/shortenMomentOutput.ts
@@ -2,9 +2,12 @@ export default function(momentOutput: string): string {
   return momentOutput
     .replace(/a\sday/, '1d')
     .replace(/\sdays/, 'd')
-    .replace(/\shours?/, 'h')
-    .replace(/\sminutes?/, 'm')
-    .replace(/\smonths?/, 'mo')
-    .replace(/\sseconds?/, 's')
-    .replace(/\syears?/, 'y')
+    .replace(/an\shour/, '1h')
+    .replace(/\shours/, 'h')
+    .replace(/a\sminute/, '1m')
+    .replace(/\sminutes/, 'm')
+    .replace(/a\smonth/, '1mo')
+    .replace(/\smonths/, 'mo')
+    .replace(/a\syear?/, '1y')
+    .replace(/\syears/, 'y')
 }
