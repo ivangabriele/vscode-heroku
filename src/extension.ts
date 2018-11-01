970bfc9ca4526873f47fac8565f22333f4feb06a
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-01 01:00:00 +0100

    extension: link linkWorkspace command

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,17 @@
 import * as vscode from 'vscode'
 
 import HerokuStatus from './components/HerokuStatus'
+import linkWorkspace from './lib/linkWorkspace'
 
 export function activate(context: vscode.ExtensionContext) {
   new HerokuStatus()
+
+  const linkWorkspaceDisposable = vscode.commands.registerCommand(
+    'extension.vscode-heroku.linkWorkspace', () => linkWorkspace())
+
+  context.subscriptions.push(
+    linkWorkspaceDisposable,
+  )
 }
 
 export function deactivate() {}
