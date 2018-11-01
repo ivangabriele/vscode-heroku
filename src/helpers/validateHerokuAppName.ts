eabef106997a342f4c16f54b4b56ca1f8b05bdec
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-11-01 01:00:11 +0100

    helpers/validateHerokuAppName: remove

--- src/helpers/validateHerokuAppName.ts
+++
@@ -1,6 +0,0 @@
-export default function validateHerokuAppName(message: string | null | undefined): boolean {
-  return  message !== undefined &&
-          message !== null &&
-          message.trim() !== '' &&
-          /^[a-z0-9\-]{3,}$/.test(message)
-}
