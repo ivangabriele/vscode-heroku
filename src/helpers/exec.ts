b92984a9628b2b1e6db808b2003a38754d686fe8
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-01-14 11:48:38 +0100

    helpers/exec: switch to exec

--- src/helpers/exec.ts
+++ src/helpers/exec.ts
@@ -1,21 +1,17 @@
-import { spawn, SpawnOptions } from 'child_process'
+import { exec, ExecOptions } from 'child_process'
 
 export default async function (
   command: string,
   args: string[] = [],
-  options: SpawnOptions = {},
+  options: ExecOptions = {},
 ): Promise<any> {
   return new Promise((resolve, reject) => {
     let res, stderr = '', stdout = ''
 
     try {
-      const batch = spawn(command, args, options)
+      const batch = exec([command].concat(args).join(' '), options)
 
-      batch.stdout.on('data', function (data) {
-        stdout += data.toString()
-      })
-
-      batch.stderr.on('data', data => stdout += data.toString())
+      batch.stdout.on('data', data => stdout += data.toString())
       batch.stderr.on('data', data => stderr += data.toString())
 
       batch.on('close', function () {
