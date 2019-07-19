68d23955e128936e430e3e2474a8d8fdc6db1441
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-19 15:34:03 +0200

    extension: skip start when not in a workspace

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,9 @@ import HerokuStatus from './components/HerokuStatus'
 import linkWorkspace from './lib/linkWorkspace'
 
 export function activate(context: vscode.ExtensionContext) {
+  // Skip extension start if we can't detect a workspace:
+  if (vscode.workspace.workspaceFolders === undefined) return;
+
   new HerokuStatus()
 
   const linkWorkspaceDisposable = vscode.commands.registerCommand(
