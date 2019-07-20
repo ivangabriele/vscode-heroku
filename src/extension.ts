d5ce3295d7644c24086be3f89ff5f62fd7207e52
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-07-20 05:55:55 +0200

    extension: add Heroku CLI availability check

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,42 @@
-import * as vscode from 'vscode'
+import { sync as commandExistsSync } from 'command-exists';
+import { commands, ExtensionContext, Uri, window, workspace } from 'vscode';
 
-import HerokuStatus from './components/HerokuStatus'
-import linkWorkspace from './lib/linkWorkspace'
+import HerokuStatus from './components/HerokuStatus';
+import linkWorkspace from './lib/linkWorkspace';
 
-export function activate(context: vscode.ExtensionContext) {
+const NO_HEROKU_ERROR_ACTION = 'Download and install';
+// tslint:disable-next-line: max-line-length
+const NO_HEROKU_ERROR_ACTION_URL = 'https://devcenter.heroku.com/articles/heroku-cli#download-and-install';
+
+export async function activate(context: ExtensionContext) {
   // Skip extension start if we can't detect a workspace:
-  if (vscode.workspace.workspaceFolders === undefined) return;
+  if (workspace.workspaceFolders === undefined) return;
 
-  new HerokuStatus()
+  // Is Heroku CLI available ?
+  if (!commandExistsSync('heroku')) {
+    const action = await window.showWarningMessage(
+      "Heroku CLI doesn't seem to be installed. Please install it and reload VS Code.",
+      NO_HEROKU_ERROR_ACTION,
+    );
 
-  const linkWorkspaceDisposable = vscode.commands.registerCommand(
-    'extension.vscode-heroku.linkWorkspace', () => linkWorkspace())
+    if (action === NO_HEROKU_ERROR_ACTION) {
+      commands.executeCommand(
+        'vscode.open',
+        Uri.parse(NO_HEROKU_ERROR_ACTION_URL),
+      );
+    }
 
-  context.subscriptions.push(
-    linkWorkspaceDisposable,
-  )
+    return;
+  }
+
+  new HerokuStatus();
+
+  const linkWorkspaceDisposable = commands.registerCommand(
+    'extension.vscode-heroku.linkWorkspace',
+    () => linkWorkspace(),
+  );
+
+  context.subscriptions.push(linkWorkspaceDisposable);
 }
 
 export function deactivate() {}
